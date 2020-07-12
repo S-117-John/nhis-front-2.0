0c65d7a71d147572e1eb0d9cd9f8a67252dee40b
S-117-John <lmz77555@gmail.com>
2020-07-12 15:26:29 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import DefaultLayout from "./pages/layout/default";
+
+
+
+function App() {
+  return (
+      <div className="App">
+          <DefaultLayout/>
+      </div>
+  );
+}
+
+export default App;
